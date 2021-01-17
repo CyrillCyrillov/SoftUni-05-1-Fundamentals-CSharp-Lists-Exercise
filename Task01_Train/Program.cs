bd6caf15c3fc16b52b5c4a8c080e8499ee53e805
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 19:41:18 +0200

    Add files via upload

---
+++ Task01_Train/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> train = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            while(true)
+            {
+                string[] comand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if(comand[0] == "end")
+                {
+                    break;
+                }
+                
+                else
+                {
+                    if(comand[0] == "Add")
+                    {
+                        train.Add(int.Parse(comand[1]));
+                    }
+
+                    else
+                    {
+                        int additionalPassengers = int.Parse(comand[0]);
+                        for (int i = 0; i < train.Count; i++)
+                        {
+                            if(train[i] + additionalPassengers <= capacity)
+                            {
+                                train[i] += additionalPassengers;
+                                i = train.Count;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine(string.Join(' ', train));
+        }
+    }
+}
