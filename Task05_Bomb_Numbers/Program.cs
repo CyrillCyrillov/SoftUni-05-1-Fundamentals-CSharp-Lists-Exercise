78717d64d6ee29f93b3a89525c5b0e1580986f3d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 19:46:55 +0200

    Add files via upload

---
+++ Task05_Bomb_Numbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task05_Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int[] bomb = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if(numbers[i] == bomb[0])
+                {
+                    for(int j = bomb[1]; j > 0; j--)
+                    {
+                        if(i + j  <= numbers.Count - 1)
+                        {
+                            numbers.RemoveAt(i + j);
+                        }
+                    }
+
+                    int rightIndex = i;
+
+                    for (int x = bomb[1]; x > 0; x--)
+                    {
+                        if(rightIndex - x >= 0)
+                        {
+                            numbers.RemoveAt(rightIndex - x);
+                            rightIndex -= 1;
+                        }
+                    }
+
+
+                    numbers.RemoveAt(rightIndex);
+
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
