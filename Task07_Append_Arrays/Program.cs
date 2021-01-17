f203a24a438457bac13022ee5d278f9325fab075
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-17 19:49:00 +0200

    Add files via upload

---
+++ Task07_Append_Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task07_Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputString = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<string> outputString = new List<string>();
+            //List<string> outputString = {start};
+
+            for (int i = inputString.Count - 1; i >= 0; i--)
+            {
+                List<string> curentString = inputString[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                
+                for (int j = 0; j < curentString.Count; j++)
+                {
+                    outputString.Add(curentString[j]);
+                }
+            }
+            
+            
+            Console.WriteLine(string.Join(' ', outputString));
+        }
+    }
+}
